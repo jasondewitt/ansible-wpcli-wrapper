99811febbf7987ae86584df307f9976b9e1b41db
Jason DeWitt <jason.dewitt@10up.com>
2018-05-06 14:22:23 -0500

    refactor wpcli_core.py to move all functions inside the class, much cleaner error handling

--- module_utils/wpcli.py
+++ module_utils/wpcli.py
@@ -34,12 +34,16 @@ EXAMPLES = '''
 '''
 
 import os
+from ansible.module_utils.urls import *
+try:
+    import json
+except ImportError:
+    import simplejson as json
 
 def wpcli_common_arg_spec():
 
     arg_spec = dict(
         path=dict(type='str',  required=True),
-        version=dict(type='str', required=False),
         force=dict(type='str', required=False),
         network=dict(type='str', required=False),
     )
@@ -53,9 +57,7 @@ class wpcli_command(object):
 
         self.module = module
         self.path = module.params["path"]
-        self.version = module.params["version"]
         self.force = module.params["force"]
-        self.network = module.params["network"]
         self.result = {}
 
 
@@ -74,11 +76,43 @@ class wpcli_command(object):
             cmd.append('--allow-root')
         # always going to want --path on the command, so go ahead and append it now
         cmd.append( '--path=%s' % self.path )
-        if self.version:
-            cmd.append( '--version=%s' % self.version )
         if self.force:
             cmd.append( '--force' )
-        if self.network:
-            cmd.append( '--network' )
+
 
         return cmd
+    
+    def find_wp_versions(self):
+        apiurl = "https://api.wordpress.org/core/stable-check/1.0/"
+
+        try:
+            versions = json.load(open_url(apiurl))
+        except HTTPError as e:
+            res = json.load(e)
+            self.result["msg"] = "Critical error checking WordPress versions api"
+            self.result["stderr"] = res["message"]
+            self.module.fail_json(**self.result)
+        self.wp_versions = versions
+
+
+    def find_wp_latest(self):
+
+        try:
+            return [key for key in self.wp_versions.keys() if (self.wp_versions[key] == "latest")]
+        except AttributeError:
+            self.find_wp_versions()
+            return [key for key in self.wp_versions.keys() if (self.wp_versions[key] == "latest")]
+
+
+    def get_wp_version(self):
+        cmd = self.prep_command()
+        cmd.extend("core version".split())
+
+        (rc, out, err) = self.execute_command(cmd)
+        if rc == 0:
+            return out.strip()
+        else:
+            self.result["msg"] = "%s is not a WordPress install" % self.path
+            self.result["stderr"] = err
+            self.result["stdout"] = out
+            self.module.fail_json(**self.result)
