cfbe3bb898b812d31a019ba8636444e3c8657736
Jason DeWitt <jason.dewitt@10up.com>
2018-05-13 11:24:17 -0500

    remove extra default test

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -5,14 +5,6 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
-
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
-
-
 def test_wpcli_exists(host):
     wpcli = host.file("/usr/local/bin/wp")
 
